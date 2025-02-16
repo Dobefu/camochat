9b9a7a1cfab4417ccb29b3d361b5f6f39807d793
Connor van Spronssen <info@dobefu.com>
2025-02-16 13:13:25 +0100

    feat: Add a better focus state to the textarea

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,15 +1,28 @@
+import { useCallback, useState } from 'react'
+
 export default function Chat() {
+  const [isFocused, setIsFocused] = useState(false)
+
+  const focusInput = useCallback(() => setIsFocused(true), [])
+  const blurInput = useCallback(() => setIsFocused(false), [])
+
   return (
     <div className="flex flex-1 flex-col gap-8">
       <div className="flex flex-1 flex-col items-center justify-end">
         <div>MESSAGES</div>
       </div>
 
-      <form className="relative">
+      <form
+        aria-selected={isFocused}
+        className="relative rounded-xl aria-selected:outline-2"
+      >
         <div className="w-full rounded-xl border border-gray-300 shadow-inner">
           <textarea
-            className="-mb-2 w-full resize-none rounded-t-xl p-2"
+            autoFocus
+            className="-mb-2 w-full resize-none rounded-t-xl p-2 outline-none"
             placeholder="Enter a message..."
+            onFocus={focusInput}
+            onBlur={blurInput}
           />
 
           <div className="flex justify-end p-2">
