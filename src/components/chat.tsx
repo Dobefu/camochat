146e3704a94c8c38bd3f457c5d53f462f6c36277
Connor van Spronssen <info@dobefu.com>
2025-02-16 13:20:36 +0100

    feat: Add logic to focus on the textarea whenever the form is clicked

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,10 +1,12 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 export default function Chat() {
+  const inputRef = useRef<HTMLTextAreaElement>(null)
   const [isFocused, setIsFocused] = useState(false)
 
-  const focusInput = useCallback(() => setIsFocused(true), [])
-  const blurInput = useCallback(() => setIsFocused(false), [])
+  const focusInput = useCallback(() => inputRef.current.focus(), [])
+  const onFocusInput = useCallback(() => setIsFocused(true), [])
+  const onBlurInput = useCallback(() => setIsFocused(false), [])
 
   return (
     <div className="flex flex-1 flex-col gap-8">
@@ -15,14 +17,16 @@ export default function Chat() {
       <form
         aria-selected={isFocused}
         className="relative rounded-xl aria-selected:outline-2"
+        onClick={focusInput}
       >
         <div className="w-full rounded-xl border border-stone-300 shadow-inner dark:border-stone-600">
           <textarea
             autoFocus
             className="-mb-2 w-full resize-none rounded-t-xl p-2 outline-none"
             placeholder="Enter a message..."
-            onFocus={focusInput}
-            onBlur={blurInput}
+            onFocus={onFocusInput}
+            onBlur={onBlurInput}
+            ref={inputRef}
           />
 
           <div className="flex justify-end p-2">
